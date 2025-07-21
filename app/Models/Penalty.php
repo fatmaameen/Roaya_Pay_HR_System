c26c2a51244929dd12427dea0826277e570e71f2
Fahd.Abuoolila <fahdabuoolila.px@gmail.com>
2025-07-21 15:30:01 +0300

    ok

--- app/Models/Penalty.php
+++ app/Models/Penalty.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Penalty extends Model
 {
     protected $guarded=[];
-    
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
